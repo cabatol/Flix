63102f1337ff68cec81864133f1bb5404b19cea4
cabatol <chino.abatol@gmail.com>
2019-02-07 19:10:54 -0800

    Update MovieDetailsViewController.swift

--- Flix/MovieDetailsViewController.swift
+++ Flix/MovieDetailsViewController.swift
@@ -33,7 +33,7 @@ class MovieDetailsViewController: UIViewController {
         
         posterView.af_setImage(withURL: posterUrl!)
         
-        let backdropPath = movie["poster_path"] as! String
+        let backdropPath = movie["backdrop_path"] as! String
         let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
         
         backdropView.af_setImage(withURL: backdropUrl!)
