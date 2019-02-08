ccc5525bea731c67bf2de71cb1cd860f4f8c534e
chino abatol <chinoabatol@chinos-Mac.local>
2019-02-07 16:12:16 -0800

    adding stuff

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by chino abatol on 2/7/19.
+//  Copyright © 2019 chino abatol. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -1,4 +1,3 @@
-https://api.themoviedb.org/3/movie/297762/videos?api_key=9fef6d15dd359a45917aaadb77e49c7e&language=en-US//
 //  MovieGridViewController.swift
 //  Flix
 //
@@ -7,16 +6,61 @@ https://api.themoviedb.org/3/movie/297762/videos?api_key=9fef6d15dd359a45917aaad
 //
 
 import UIKit
+import AlamofireImage
 
-class MovieGridViewController: UIViewController {
+class MovieGridViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var movies = [[String:Any]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/297762/videos?api_key=9fef6d15dd359a45917aaadb77e49c7e&language=en-US")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                self.movies = dataDictionary["results"] as! [[String:Any]]
+                
+                self.collectionView.reloadData()
+            }
+        }
+        task.resume()
 
         // Do any additional setup after loading the view.
     }
     
-    //https://api.themoviedb.org/3/movie/297762/videos?api_key=9fef6d15dd359a45917aaadb77e49c7e&language=en-US
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGridCell", for: indexPath) as! MovieGridCell
+        
+        let movie = movies[indexPath.item]
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        cell.posterView.af_setImage(withURL: posterUrl!)
+        
+        return cell
+    }
+    
     
 
     /*
